44c220e9af8ecdcff4667a3e06a10d4d16917a47
Andrés <andres.canales66@gmail.com>
2022-04-01 17:34:14 -0600

    Adding devise to manage users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'home#index'
   resources :projects, only: [:index, :new, :create]
   resources :clients, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
